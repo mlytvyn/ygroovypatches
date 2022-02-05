e574ce9ccfcf2730e45f618ed08b776fbb5c33e1
Mykhailo Lytvyn <>
2022-02-05 20:06:08 +0100

    - added default extension for `CurrentEnvironmentProvider`

---
+++ src/ygroovypatchespackage/context/patch/YGroovypatchesCurrentEnvironmentProvider.java
@@ -0,0 +1,12 @@
+package ygroovypatchespackage.context.patch;
+
+import com.github.mlytvyn.patches.groovy.EnvironmentEnum;
+import com.github.mlytvyn.patches.groovy.context.patch.DefaultCurrentEnvironmentProvider;
+
+public class YGroovypatchesCurrentEnvironmentProvider extends DefaultCurrentEnvironmentProvider {
+
+    @Override
+    public EnvironmentEnum getCurrentEnvironment() {
+        return super.getCurrentEnvironment();
+    }
+}
